9c5e2927ce2f835af0e962c6cee594f10a272366
John Oram <john@oram.ca>
2023-03-20 16:20:12 -0700

    bump

--- require_prefixed_ids/main.py
+++ require_prefixed_ids/main.py
@@ -1,13 +1,19 @@
 import argparse
+import django
 from typing import List
 
 
-def print_arguments(filenames: List[str]):
-    for filename in filenames:
-        filename = filename.replace(".py", "").replace("/", ".")
-        i = __import__(filename)
-        print(dir(i))
+def get_django_models(filenames: List[str]):
+    from django.apps import apps
+    print(apps.all_models.keys())
+    for app_name in apps.all_models.keys():
+        for model in apps.get_app_config(app_name).get_models():
+            yield model
 
+
+def verify_ids_are_prefixed(filenames: List[str]):
+    models = get_django_models(filenames)
+    print(list(models))
     # suggest `id = CustomShortUUID(.....)` in the output
     # try and make it generic, algorithm based instead of a hardcoded list.
 
@@ -17,7 +23,7 @@ def main():
     parser.add_argument("filenames", nargs="*")
     args = parser.parse_args()
 
-    print_arguments(args.filenames)
+    verify_ids_are_prefixed(args.filenames)
 
 
 if __name__ == "__main__":
