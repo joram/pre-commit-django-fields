921a9c3f4284494ad29e42d3d34d5936bc4323bb
John Oram <john@oram.ca>
2023-03-20 15:31:07 -0700

    bump

--- require_prefixed_ids/main.py
+++ require_prefixed_ids/main.py
@@ -1,5 +1,18 @@
+import argparse
+from typing import List
+
+
+def print_arguments(arguments: List[str]):
+    for argument in arguments:
+        print(argument)
+
+
 def main():
-    print("Running main.py")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*")
+    args = parser.parse_args()
+
+    print_arguments(args.filenames)
 
 
 if __name__ == "__main__":
